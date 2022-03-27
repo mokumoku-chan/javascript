7f2767b588666af7c677faa85c26fb1a4901ffe0
mokumoku-chan <maho.kumo1997@gmail.com>
2022-03-27 23:37:36 +0900

    Study if_else

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ let int1 = 1;
 let str1 = 'JavaScriptを覚えよう'
 
 alert(hello); alert(int1); alert(str1);
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple) {
+  alert('みかんとリンゴが同じ値段');
+} else{
+  alert('みかんの値段がリンゴより高い');
+}
